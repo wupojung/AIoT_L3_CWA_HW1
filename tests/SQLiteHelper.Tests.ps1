4abc705e8c1ffac8a304e60bbe9974546a493acf
wupolung <wupojung@gmail.com>
2026-09-25 01:11:51 +0800

    test: migrate SQLite tests to Pester 6 assertions

--- tests/SQLiteHelper.Tests.ps1
+++ tests/SQLiteHelper.Tests.ps1
@@ -10,17 +10,21 @@ Describe "SQLiteHelper Tests" {
     It "Can create a table and insert data" {
         $createSql = "CREATE TABLE test_table (id INT, name TEXT);"
         $insertSql = "INSERT INTO test_table VALUES (1, 'TestName');"
-        
-        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $createSql } | Should Not Throw
-        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $insertSql } | Should Not Throw
-        
-        Test-Path "$PSScriptRoot\test_db.sqlite" | Should Be $true
+
+        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $createSql } |
+            Should-NotThrow
+
+        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $insertSql } |
+            Should-NotThrow
+
+        (Test-Path "$PSScriptRoot\test_db.sqlite") | Should-BeTrue
     }
 
     It "Throws exception on invalid SQL" {
         $invalidSql = "INSERT INTO non_existent_table VALUES (1);"
-        
-        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $invalidSql } | Should Throw "SQLite Error:"
+
+        { Invoke-SqliteQuery -DatabasePath "$PSScriptRoot\test_db.sqlite" -Query $invalidSql } |
+            Should-Throw -ExceptionMessage "*SQLite Error:*"
     }
 
     AfterAll {
